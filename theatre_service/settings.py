6f7ccd79ccc09e24a5b6b59e029110dfd92e7335
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-07-13 14:03:01 +0200

    feat: configure project settings and urls

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "theatre",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
