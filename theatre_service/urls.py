01f81bb045e3e4fe66190a1cf4e9a470c1fd88be
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-07-14 13:57:35 +0200

    fix: theatre path

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/theatre", include("theatre.urls", namespace="theatre")),
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
     path("api/user/", include("user.urls", namespace="user")),
 ]
