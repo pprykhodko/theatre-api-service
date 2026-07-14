c282ec4b2dbb9b6d05ec513f2c855d2c661ae10f
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-07-14 13:58:44 +0200

    feat: configure roots for actors, genres and plays

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from theatre.views import ActorViewSet, PlayViewSet, GenreViewSet
+
 router = routers.DefaultRouter()
+
+router.register("actors", ActorViewSet, basename="actors")
+router.register("genres", GenreViewSet, basename="genres")
+router.register("plays", PlayViewSet, basename="plays")
 
 urlpatterns = [path("", include(router.urls))]
 
