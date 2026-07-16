75b2601c899bc81fc46ca58e32c883413082cd70
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-07-16 16:53:11 +0200

    feat: implement search by title, genres, actors and minor changes to PlayViewSet

--- theatre/views.py
+++ theatre/views.py
@@ -1,7 +1,13 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 
 from theatre.models import Actor, Genre, Play
-from theatre.serializers import ActorSerializer, GenreSerializer, PlaySerializer
+from theatre.serializers import (
+    ActorSerializer,
+    GenreSerializer,
+    PlaySerializer,
+    PlayListSerializer,
+    PlayDetailSerializer
+)
 
 
 class ActorViewSet(viewsets.ModelViewSet):
@@ -14,10 +20,38 @@ class GenreViewSet(viewsets.ModelViewSet):
     serializer_class = GenreSerializer
 
 
-class PlayViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    viewsets.GenericViewSet,
-):
-    queryset = Play.objects.all()
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.prefetch_related("genres", "actors")
     serializer_class = PlaySerializer
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        title = self.request.query_params.get("title")
+        genres = self.request.query_params.get("genres")
+        actors = self.request.query_params.get("actors")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if genres:
+            genres_ids = self._params_to_ints(genres)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        if actors:
+            actors_ids = self._params_to_ints(actors)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+        return PlaySerializer
