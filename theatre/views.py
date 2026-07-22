9c3160f216ee0d97886ac17e9969eee9b6fc80c1
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-07-22 13:51:18 +0200

    feat: add filters for Actor

--- theatre/views.py
+++ theatre/views.py
@@ -58,6 +58,20 @@ class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
+    def get_queryset(self):
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        queryset = super().get_queryset()
+
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+
+        return queryset.distinct()
+
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
